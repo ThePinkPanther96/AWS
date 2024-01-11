7dafb803682dda6c121839f4d5d8bdf11622ac01
git <git@192.168.1.51>
2024-01-11 19:26:39 +0200

    Fixed Deploy.ps1, added Synopsys, new version of Winfsp

--- Map S3 as a network drive  - Windows/Deploy.ps1
+++ Map S3 as a network drive  - Windows/Deploy.ps1
@@ -1,9 +1,121 @@
+<#
+.SYNOPSIS
+This script automates the setup of an AWS S3 mapping to a Windows Network Drive using Rclone.
+It includes functions to download necessary files from an S3 bucket, install required software,
+and create a scheduled task to run a PowerShell script at logon.
+
+.NOTES
+File Name      : Mount.ps1
+Prerequisite   : PowerShell, AWS Tools for PowerShell
+Dependencies   : AWSPowerShell
+Copyright 2024 - Gal Rozman
+
+
+.FUNCTIONALITY
+
+function Get-Files {
+
+    .SYNOPSIS
+    Downloads files from an AWS S3 bucket to specified target paths.
+
+    .DESCRIPTION
+    The Get-Files function downloads files from an AWS S3 bucket and checks their existence at the target paths.
+    If the files already exist, it prints a message; otherwise, it downloads and verifies their deployment.
+    This function for deploying the needed object from a S3 Bucket.
+
+    .PARAMETER TargetPaths
+    An array of the full target path including the objects names, where the files should be deployed.
+    It is important to specify the full target path including the precise name of the object as it appears in the deployment bucket.
+    
+    .PARAMETER SecretKey
+    The AWS IAM User Secret Acsses key.
+
+    .PARAMETER Key
+    The AWS IAM User Acsses key.
+
+    .PARAMETER BucketName
+    The name of the AWS S3 bucket.
+}
+
+function Install-Software {
+
+    .SYNOPSIS
+    Installs software using the Windows Installer (msiexec.exe) and checks for successful installation.
+    This is applicable only for MSI installations.
+    
+    .DESCRIPTION
+    The Install-Software function installs a specified software using msiexec.exe.
+    It checks if the software is already installed; if not, it attempts installation.
+    It retries the installation on failure and reports the outcome. 
+    In this case, the software to install is WinFsp.
+
+    .PARAMETER Software
+    The name of the software to be installed.
+    Only the software name itself is required; the full name of the software is not needed. 
+    For example, if the software name is 'winfsp-2.0.23075.msi,' only the 'WinFsp' part is needed. 
+    This precision ensures the code's accuracy.
+
+    .PARAMETER TargetPath
+    The target directory where the software installer is located.
+    In this case the objects will be deployed to "C:\Rclone" 
+    WinFsp will be deployed to "C:\Windows\Temp"
+}
+
+function Set-Task {
+
+    .SYNOPSIS
+    Creates a scheduled task to run a PowerShell script at logon.
+
+    .DESCRIPTION
+    The Set-Task function configures a scheduled task named 'Rclone' to run a specified PowerShell script at logon.
+
+    .PARAMETER TaskName
+    The name of the scheduled task.
+
+    .PARAMETER Description
+    The description of the scheduled task.
+
+    .PARAMETER Arguments
+    The arguments to be passed to the PowerShell script. 
+
+    .PARAMETER User
+    The user account under which the task will run.
+
+}
+
+.EXAMPLE
+$TargetPaths is a list of the deployment destinations on the local machine. 
+To deploy correctly, you must specify the path for deployment, 
+as well as the precise names of the objects as they are named in the deployment S3 Bucket
+See example:
+
+$TargetPaths = @("C:\Rclone\Rclone\rclone.conf", "C:\Rclone\Rclone\rclone.exe", 
+"C:\Windows\Temp\winfsp-2.0.23075.msi", "C:\Rclone\Mount.ps1")
+
+Example for executing the script:
+
+if ($MyInvocation.CommandOrigin -eq 'Runspace'){
+
+    Get-Files -TargetPaths $TargetPaths `
+        -BucketNmae "testbucket" `
+        -Key "hvhgHG787JF%67g$^" `
+        -SecretKey "&^6$%$#^&V%^$c%4^5C8&b87^O7V" `
+    
+    Install-Software -Software "winfsp" -TargetPath "C:\Windows\Temp\"
+    
+    Set-Task -TaskName Rclone deployment `
+        -Description Deploy Rclone s3 bucket to win drive. `
+        -Arguments "-WindowStyle hidden -file C:\Rclone\Mount.ps1" `
+        -User SYSTEM
+}
+#>
+
 function Get-Files {
     param (
         [string[]]$TargetPaths,
         [string]$SecretKey,
         [string]$Key,
-        [string]$BucketName = "systemvalidation"
+        [string]$BucketName
     )
     foreach ($Path in $TargetPaths) {
         if (Test-Path -Path $Path) {
@@ -102,10 +214,10 @@ $TargetPaths = @("C:\Rclone\Rclone\rclone.conf", "C:\Rclone\Rclone\rclone.exe",
 
 if ($MyInvocation.CommandOrigin -eq 'Runspace'){
 
-    Get-Files -TargetPaths $TargetPaths -S3Objects $S3Objects `
-        -BucketNmae $BucketName `
-        -Key "AKIAR2QC6VD465F4O4PH" `
-        -SecretKey "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" `
+    Get-Files -TargetPaths $TargetPaths `
+        -BucketNmae "BUCKET NAME" `
+        -Key "KEY" `
+        -SecretKey "SECRET KEY" `
     
     Install-Software -Software "winfsp" -TargetPath "C:\Windows\Temp\"
     
