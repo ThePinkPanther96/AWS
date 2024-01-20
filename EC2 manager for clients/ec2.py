3303471322f403ebe1754be590fcbf6344c97d20
git <git@192.168.1.51>
2024-01-21 01:35:18 +0200

    status

--- EC2 manager for clients/ec2.py
+++ EC2 manager for clients/ec2.py
@@ -1,6 +1,31 @@
 import boto3
 import json
 
+with open('./config.json', 'r') as f:
+    config = json.load(f)
+
+access_key = config["access_key"]
+secret_access_key = config["secret_access_key"]
+region = config["region"]
+name_list = config["id_list"]
+
+ec2 = boto3.client('ec2', aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name=region)
+
+def ec2_status(names):
+    response = ec2.describe_instances(
+        Filters=[
+            {'Name': 'tag:Name', 'Values': names}
+        ]
+    )
+    if 'Reservations' in response:
+        for reservation in response['Reservations']:
+            for instance in reservation['Instances']:
+                state = instance['State']['Name']
+                if state == 'running':
+                    return True 
+    return False
+
+
 def start_ec2(names):
     instances_to_start = []
     response = ec2.describe_instances(
@@ -8,7 +33,6 @@ def start_ec2(names):
             {'Name': 'tag:Name', 'Values': names}
         ]
     )
-
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
             instances_to_start.append(instance['InstanceId'])
@@ -24,7 +48,6 @@ def stop_ec2(names):
             {'Name': 'tag:Name', 'Values': names}
         ]
     )
-
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
             instances_to_stop.append(instance['InstanceId'])
@@ -32,25 +55,17 @@ def stop_ec2(names):
     response = ec2.stop_instances(InstanceIds=instances_to_stop)
     print(response)
 
-#------------------------------------------------------------------------------------------
-
-with open('./config.json', 'r') as f:
-    config = json.load(f)
-
-access_key = config["access_key"]
-secret_access_key = config["secret_access_key"]
-region = config["region"]
-name_list = config["id_list"]
-
-ec2 = boto3.client('ec2', 
-                   aws_access_key_id=access_key, 
-                   aws_secret_access_key=secret_access_key, 
-                   region_name=region
-                   )
 
 #------------------------------------------------------------------------------------------
 
+result = ec2_status(name_list)
 
-#start_ec2(name_list)
-stop_ec2(name_list)
+if result:
+    print("At least one instance is running.")
+else:
+    print("No instances are running.")
+#------------------------------------------------------------------------------------------
+ec2_status(name_list)
+start_ec2(name_list)
+#stop_ec2(name_list)
 
