f300bbd89febd8fe00b5548dbd38b125c5a77ac5
git <git@192.168.1.51>
2024-01-20 19:36:01 +0200

    fix

--- EC2 manager for clients/ec2.py
+++ EC2 manager for clients/ec2.py
@@ -1,7 +1,7 @@
 import boto3
 import json
 
-with open('./config.json', 'r') as f:
+with open("config.json", 'r') as f:
     config = json.load(f)
 
 access_key = config["access_key"]
@@ -9,7 +9,13 @@ secret_access_key = config["secret_access_key"]
 region = config["region"]
 name_list = config["id_list"]
 
-ec2 = boto3.client('ec2', aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name=region)
+ec2 = boto3.client('ec2', 
+                   aws_access_key_id=access_key, 
+                   aws_secret_access_key=secret_access_key, 
+                   region_name=region
+                   )
+
+
 
 
 def start_ec2(names):
@@ -28,10 +34,8 @@ def start_ec2(names):
     print(response)
 
 
-
-
 def stop_ec2(names):
-    instances_to_start = []
+    instances_to_stop = []
     response = ec2.describe_instances(
         Filters=[
             {'Name': 'tag:Name', 'Values': names}
@@ -40,11 +44,13 @@ def stop_ec2(names):
 
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
-            instances_to_start.append(instance['InstanceId'])
+            instances_to_stop.append(instance['InstanceId'])
 
-    response = ec2.stop_instances(InstanceIds=instances_to_start)
+    response = ec2.stop_instances(InstanceIds=instances_to_stop)
     print(response)
 
+#------------------------------------------------------------------------------------------
 
-#start_ec2(name_list)
-stop_ec2(name_list)
+start_ec2(name_list)
+#stop_ec2(name_list)
+
